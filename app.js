f44dae4671b552cd1837816580fd7e0e523fbdee
vinigomes <vinigomes@outlook.com>
2017-06-19 11:21:02 -0300

    'post'

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -21,7 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Route that receives a POST request to /
+app.post('/', function (req, res) {
+    const body = req.body.Body;
+    console.log('[BODY] ' + body);
+    res.set('Content-Type', 'text/plain');
+    res.status(200).send('You sent: ${body} to Express');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
