e337bbb961fc774e3ccd9c76d3e4e635bda08341
vinigomes <vinigomes@outlook.com>
2017-06-20 15:13:16 -0300

    adjust

--- src/Dao.js
+++ src/Dao.js
@@ -6,13 +6,12 @@ var url = process.env.MONGODB_URI;
 class Dao {
 
     save(lead) {
-        console.log(lead.id);
         MongoClient.connect(url, function (err, db) {
             if (err) {
                 console.log('Unable to connect to the mongoDB server. Error:', err);
             } else {
                 db.collection('leads').insertOne( {lead}, function(err, result) {
-                    console.log("Inserted a document into the lead collection.");
+                    console.log("Inserted a document into the lead collection with id: " + lead.id);
                     db.close();
                 });
             }
