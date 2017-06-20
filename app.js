447336ac26209713d70983c44ef5680588097d8b
vinigomes <vinigomes@outlook.com>
2017-06-20 11:50:24 -0300

    bug

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var index = require('./routes/index');
 var app = express();
 var dao = require('./src/dao');
 
