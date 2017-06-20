677dbdeb6c01de07185e65bf7d55e4c52dca0ca7
vinigomes <vinigomes@outlook.com>
2017-06-20 14:35:01 -0300

    bug

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.post('/rd-webhook', function (req, res) {
     var leads = body["leads"];
     var dao = new Dao();
     for(var lead in leads) {
-        console.log(leads[lead]);
         console.log(leads[leads]['lead']);
         dao.save(leads[lead]);
     }
