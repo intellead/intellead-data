1632e4023c3c04395193ea013dbffaddb3bfd03a
vinigomes <vinigomes@outlook.com>
2017-06-20 11:32:45 -0300

    bug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var dao = require('../src/dao');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'rdstation-webhook' });
 });
+
+router.get('/rd-webhook', function(req, res, next) {
+    var body = req.body;
+    if (!body) return res.sendStatus(400);
+    var leads = body["leads"];
+    for(var lead in leads) {
+        dao.save(leads[lead]);
+    }
+    res.sendStatus(200);
+});
+
 
 module.exports = router;
