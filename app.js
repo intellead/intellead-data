9639f0dc28e9e844463f4319d08824e91a460aa6
vinigomes <vinigomes@outlook.com>
2017-06-28 17:14:53 -0700

    all-leads

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+var router = express.Router();
 var app = express();
 var Dao = require('./src/Dao');
 
