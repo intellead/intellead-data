acedb0628ebac6b354bc5490ff3aa57c2f2269d7
vinigomes <vinigomes@outlook.com>
2017-07-24 17:06:20 -0700

    test

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/rd-webhook', function (req, res) {
                     'content-type': 'application/x-www-form-urlencoded',
                     'cache-control': 'no-cache'
                 },
-                data: json_enrich
+                body: json_enrich
             }, function (error, response, body) {
                 if (error){
                     console.log(error);
