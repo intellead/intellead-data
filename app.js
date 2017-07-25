1f7dd8b899d303bb93a0174fba888ec1ac10d4e4
vinigomes <vinigomes@outlook.com>
2017-07-24 17:28:22 -0700

    test

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/rd-webhook', function (req, res) {
                     'content-type': 'application/x-www-form-urlencoded',
                     'cache-control': 'no-cache'
                 },
-                body: JSON.stringify(json_enrich),
+                body: '{lead_id: 151680927}',
                 data: json_enrich
             }, function (error, response, body) {
                 if (error){
