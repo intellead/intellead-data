866b748d9b6629be7ac7f7a7d2cc62db7944de29
Vinícius Gomes <vinigomes@Viniciuss-MacBook-Pro.local>
2017-07-28 23:35:56 -0700

    test

--- app.js
+++ app.js
@@ -56,7 +56,20 @@ router.get('/rd-webhook', function(req, res, next) {
     res.sendStatus(200);
 });
 
-app.post('/all-leads', function(req, res){
+// app.post('/all-leads', function(req, res){
+//     var page_number = parseInt(req.body.page_number),
+//         page_size = parseInt(req.body.page_size);
+//     new Dao().findAllLeads(page_number, page_size, function (err, result) {
+//         if (err) {
+//             return res.sendStatus(400);
+//         }
+//         if (result) {
+//             return res.status(200).send(result);
+//         }
+//     });
+// });
+
+router.post('/all-leads', function(req, res){
     var page_number = parseInt(req.body.page_number),
         page_size = parseInt(req.body.page_size);
     new Dao().findAllLeads(page_number, page_size, function (err, result) {
